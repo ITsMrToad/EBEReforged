4205c27cf66fe2c7af593c405b207128cebdac8e
ITsMrToad <121745579+ITsMrToad@users.noreply.github.com>
2024-08-28 10:56:30 +0500

    LevelRendererMixin now works 🎉 | 0.4

--- src/main/java/foundationgames/enhancedblockentities/core/mixin/LevelRendererMixin.java
+++ src/main/java/foundationgames/enhancedblockentities/core/mixin/LevelRendererMixin.java
@@ -14,23 +14,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import org.jetbrains.annotations.Nullable;
 
-@Mixin(value = LevelRenderer.class)
+@Mixin(LevelRenderer.class)
 public abstract class LevelRendererMixin {
 
-    @Nullable @Unique public ChunkRenderDispatcher.RenderChunk ebe$chunk;
-
-    @Inject(method = "compileChunks", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/SectionPos;of(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/core/SectionPos;", shift = At.Shift.AFTER))
-    public void addPostRebuildTask(Camera camera, CallbackInfo ci) {
-        if (!EBEOtherUtils.CHUNK_UPDATE_TASKS.isEmpty() && this.ebe$chunk != null) {
-            SectionPos pos = SectionPos.of(this.ebe$chunk.getOrigin());
+    @ModifyVariable(method = "compileChunks", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/SectionPos;of(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/core/SectionPos;", shift = At.Shift.AFTER), index = 8)
+    public ChunkRenderDispatcher.RenderChunk addPostRebuildTask(ChunkRenderDispatcher.RenderChunk value) {
+        if (!EBEOtherUtils.CHUNK_UPDATE_TASKS.isEmpty() ) {
+            SectionPos pos = SectionPos.of(value.getOrigin());
 
             if (EBEOtherUtils.CHUNK_UPDATE_TASKS.containsKey(pos)) {
                 Runnable task = EBEOtherUtils.CHUNK_UPDATE_TASKS.remove(pos);
-                ((ChunkRenderTaskAccess) this.ebe$chunk).ebe$setTaskAfterRebuild(task);
+                ((ChunkRenderTaskAccess) value).ebe$setTaskAfterRebuild(task);
             }
         }
+        return value;
     }
-
+    
     @Inject(method = "addRecentlyCompiledChunk", at = @At("HEAD"))
     private void runPostRebuildTask(ChunkRenderDispatcher.RenderChunk chunk, CallbackInfo ci) {
         ((ChunkRenderTaskAccess) chunk).runAfterRebuildTask();
